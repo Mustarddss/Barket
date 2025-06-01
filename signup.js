3b21215dbdf2411ce7b797152af9534179629501
Mustarddss <160338825+Mustarddss@users.noreply.github.com>
2025-06-02 07:50:35 +0800

    Create signup.js

---
+++ signup.js
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>NU Barket LogIn</title>
+    <link rel="stylesheet" href="Signup.css">
+</head>
+<body> 
+    <div class="glass-container">
+        <div class="logo">
+            <img src="Project Logo.png" alt="Logo">
+        </div>
+        <div class="logo-text">
+             <span>NU Barket</span>
+        </div>
+
+        <h2>Register Here!</h2>
+        <form>
+            <div class="input-group">
+                <input type="text" required>
+                <label>First Name</label>
+            </div>
+
+            <div class="input-group">
+                <input type="text" required>
+                <label>Last Name</label>
+            </div>
+
+            <div class="input-group">
+                <input type="text" required>
+                <label>School Email</label>
+            </div>
+
+            <div class="input-group">
+                <input type="password" required>
+                <label>Password</label>
+            </div>
+
+            <div class="input-group">
+                <input type="password" required>
+                <label>Confirm Password</label>
+            </div>
+
+            <button type="submit" class="register-btn">Register</button>
+            <div class="login-link">
+                <p>Ready to shop? <a href="Login.html">Login</a></p>
+        </form>
+    </div>
+    <script src="signup.js"></script>
+</body>
+</body>
+</html>
